ef98b6076ac29ab9900fd9971734344f3cd793e8
Mikhail <mr.kosheef54@gmail.com>
2024-03-26 00:45:51 +0500

    first version

--- internal/flood_contorol.go
+++ internal/flood_contorol.go
@@ -2,34 +2,59 @@ package internal
 
 import (
 	"context"
+	"errors"
 	"github.com/redis/go-redis/v9"
 	"strconv"
-	"task/db"
-	"task/lib"
+	"time"
 )
 
-type Controller struct{}
+const (
+	defaultMeetings   int64 = 10
+	defaultTimeToLive       = 2 * time.Minute
+)
 
-var client *redis.Client = nil
+var meetingsExceeded = errors.New("maximum number of meetings was exceeded")
+
+type Controller struct {
+	client      *redis.Client
+	maxMeetings int64
+	timeToLive  time.Duration
+}
+
+func NewController(client *redis.Client, amount int64, timeToLive time.Duration) Controller {
+	if amount < 0 {
+		amount = defaultMeetings
+	}
+	if timeToLive < 0 {
+		timeToLive = defaultTimeToLive
+	}
+	return Controller{client: client, maxMeetings: amount, timeToLive: timeToLive}
+}
 
 func (ctr Controller) Check(ctx context.Context, userID int64) (bool, error) {
-	if client == nil {
-		newClient, err := db.Initialize()
-		if err != nil {
-			return false, err
-		}
-		client = newClient
-	}
-
-	var person lib.UserInfo
 	stringId := strconv.FormatInt(userID, 36)
-	err := client.Get(ctx, stringId).Scan(&person)
-	if err != nil {
-		return false, err
-	}
+	_, err := ctr.client.Pipelined(ctx, func(pipe redis.Pipeliner) error {
+		pipeline := ctr.client.Pipeline()
+		amount, err := pipeline.Incr(ctx, stringId).Result()
+		if err != nil {
+			return err
+		} else if amount > ctr.maxMeetings {
+			return meetingsExceeded
+		}
 
-	person.MeetsAmount += 1
-	if err := client.Set(ctx, stringId, person, 0).Err(); err != nil {
+		curTTL, err := pipeline.TTL(ctx, stringId).Result()
+		if err != nil {
+			return err
+		}
+		if curTTL < 0 {
+			pipeline.Expire(ctx, stringId, ctr.timeToLive)
+		}
+		return nil
+	})
+	if err != nil {
+		if errors.Is(meetingsExceeded, err) {
+			return false, nil
+		}
 		return false, err
 	}
 
--- lib/table.go
+++
@@ -1,5 +0,0 @@
-package lib
-
-type UserInfo struct {
-	MeetsAmount int `redis:"meets"`
-}
